cc162f3eae69908d9d309ea08c0cd7023553ae91
Amit-TheOne <amit.dew009@gmail.com>
2024-09-25 19:55:12 +0530

    add navbar, types and action button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import { useState, useEffect } from "react";
+import Navbar from "@/components/navbar";
+import { SelectedPage } from "@/utils/types";
+
 function App() {
-    return <div className="app bg-gray-20"> app</div>;
+    const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+    const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        if (window.scrollY === 0) {
+            setIsTopOfPage(true);
+            setSelectedPage(SelectedPage.Home);
+        }
+        if (window.scrollY !== 0) {
+            setIsTopOfPage(false);
+        }
+      }
+
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+    }, [])
+    
+
+    return ( 
+        <div className="app bg-gray-20">
+            <Navbar 
+            isTopOfPage={isTopOfPage}
+            selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+        </div>
+    );
 }
 
 export default App;
